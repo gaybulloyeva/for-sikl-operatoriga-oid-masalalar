7b9d281cf352bb622a1f0f3a347fc7e16847f91c
new <gaybullayevamrayhona718@gmail.com>
2024-08-26 12:55:23 +0500

    for16

---
+++ for16/script.js
@@ -0,0 +1,13 @@
+function solve(a, n) {
+
+    for (let i = 1; i <= n; i++) {
+        let result = a ** i
+
+        console.log(a + " ning " + i + "-darajasi " + result)
+    }
+
+
+}
+
+
+solve(2, 5)
